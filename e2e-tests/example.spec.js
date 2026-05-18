a797abbdd4ab75a8b0aaf81794a4dc7fadda1df8
Alex Banks <44756064+dizzyant@users.noreply.github.com>
2026-05-18 10:08:48 +0100

    9. Simple end-to-end tests

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -6,4 +6,9 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test('can navigate from main page to particular pokemon',async({page}) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
